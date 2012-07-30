9a6665718d191e88b70d6261788e1448b8251d4b
My Name <phowl.mouth@gmail.com>
2012-07-30 16:50:19 -0500

    sfml_colors.nim with some default color definitions

---
+++ sfml_colors.nim
@@ -0,0 +1,12 @@
+import sfml
+let
+  Black*: TColor = newColor(0, 0, 0)
+  White*: TColor = newColor(255, 255, 255)
+  Red*: TColor = newColor(255, 0, 0)
+  Green*: TColor = newColor(0, 255, 0)
+  Blue*: TColor = newColor(0, 0, 255)
+  Yellow*: TColor = newColor(255, 255, 0)
+  Magenta*: TColor = newColor(255, 0, 255)
+  Cyan*: TColor = newColor(0, 255, 255)
+  Transparent*: TColor = newColor(0, 0, 0, 0)
+##todo: define more colors lul
