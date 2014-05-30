8dab4dfb33bf5bf7a55134086c1e080eef1f9967
fowlmouth <phowl.mouth@gmail.com>
2014-05-30 00:00:48 -0500

    added new sfml c++ wrapper

---
+++ sfml2.nim
@@ -0,0 +1,94 @@
+# SFML C++ wrapper, made possible by clibpp (babel install clibpp)
+
+when not defined(CPP):
+  {.error: "SFML is a C++ library, please compile in CPP mode or use the CSFML wrapper".}
+
+import clibpp
+
+{.passl: "-lsfml-graphics -lsfml-window -lsfml-system".}
+const
+  graphics_h = "<SFML/Graphics.hpp>"
+  window_h = "<SFML/Window.hpp>"
+
+var
+  Style_Default* {.importc:"sf::Style::Default", header:graphics_h.}: uint32
+
+#type VideoMode* {.importc:"sf::VideoMode",header:window_h.} = object
+#    VideoMode(unsigned int modeWidth, unsigned int modeHeight, unsigned int modeBitsPerPixel = 32);
+class(VideoMode, importc:"sf::VideoMode", header:window_h):
+  ##
+proc newVideoMode* (W,H: cuint; bpp = 32.cuint): VideoMode {.importc:"sf::VideoMode",header:window_h.} 
+
+type EventType*{.pure.} = enum
+  Closed,                # ///< The window requested to be closed (no data)
+  Resized,               # ///< The window was resized (data in event.size)
+  LostFocus,             # ///< The window lost the focus (no data)
+  GainedFocus,           # ///< The window gained the focus (no data)
+  TextEntered,           # ///< A character was entered (data in event.text)
+  KeyPressed,            # ///< A key was pressed (data in event.key)
+  KeyReleased,           # ///< A key was released (data in event.key)
+  MouseWheelMoved,       # ///< The mouse wheel was scrolled (data in event.mouseWheel)
+  MouseButtonPressed,    # ///< A mouse button was pressed (data in event.mouseButton)
+  MouseButtonReleased,   # ///< A mouse button was released (data in event.mouseButton)
+  MouseMoved,            # ///< The mouse cursor moved (data in event.mouseMove)
+  MouseEntered,          # ///< The mouse cursor entered the area of the window (no data)
+  MouseLeft,             # ///< The mouse cursor left the area of the window (no data)
+  JoystickButtonPressed, # ///< A joystick button was pressed (data in event.joystickButton)
+  JoystickButtonReleased,# ///< A joystick button was released (data in event.joystickButton)
+  JoystickMoved,         # ///< The joystick moved along an axis (data in event.joystickMove)
+  JoystickConnected,     # ///< A joystick was connected (data in event.joystickConnect)
+  JoystickDisconnected   # ///< A joystick was disconnected (data in event.joystickConnect)
+  
+  
+class(Event, importc:"sf::Event", header:window_h) do:
+  ##
+  var kind* {.importc:"type".}: EventType
+
+#class Window, "<SFML/Window.hpp>":
+
+class(Color, importc:"sf::Color", header:graphics_h):
+  ##
+proc newColor* (r,g,b:uint8,a=255'u8): Color {.importc:"sf::Color",header:graphics_h.}
+
+
+class(CircleShape, importc:"sf::CircleShape", header:graphics_h):
+  proc setFillColor* (color:Color) 
+  ##
+proc newCircleShape* (radius: float): CircleShape {.importc:"sf::CircleShape", header:graphics_h.}
+
+
+
+type Drawable = CircleShape # | RectangleShape | ...
+
+class(RenderWindow, importc: "sf::RenderWindow", header: window_h) do:
+  proc create* (mode:VideoMode; title:cstring; style:uint32)
+  proc isOpen* : bool
+  proc close* 
+  proc clear* 
+  proc display*
+  proc pollEvent* (dest: Event): bool
+  
+  proc draw* (obj: Drawable)
+
+# proc delete* [A](some: var A) = {.emit: "delete `some`;".} 
+
+when isMainModule:
+
+  proc main =
+    var w{.noInit.}: RenderWindow
+    w.create(newVideoMode(800,600,32), "foo", StyleDefault)
+    let shape = newCircleShape(100.0)
+    shape.setFillColor newColor(0,255,0)
+    
+    while w.isOpen:
+      var event{.noinit.}: Event
+      while w.pollEvent(event):
+        if event.kind == EventType.Closed:
+          w.close
+      
+      w.clear
+      w.draw shape
+      w.display
+
+  main()  
+
